2979ee2bd1e8dfccd28752ae9ca9ea81f646ce9a
Gabriel <gabriellippidacosta@gmail.com>
2025-05-22 19:43:14 -0300

    Criei os métodos do personagem

--- jogo/Personagem.java
+++ jogo/Personagem.java
@@ -2,19 +2,38 @@ package jogo;
 
 public class Personagem {
     String nome;
-    int energia;
-    int fome;
-    int sono;
+    int energia = 10;
+    int fome = 0;
+    int sono = 0;
 
     void cacar() {
-        System.out.println(nome + " caçando");
+        if(energia >= 2) {
+            System.out.println(nome + " caçando");
+            energia -= 2;
+        } else {
+            System.out.println(nome + " sem energia para caçar");
+        }
+        fome = Math.min(fome + 1, 10);
+        sono = Math.min(sono + 1, 10);
     }
 
     void comer() {
-        System.out.println(nome + " comendo");
+        if(fome >= 1) {
+            System.out.println(nome + " comendo");
+            energia = Math.min(energia + 1, 10);
+            fome -= 1;
+        } else {
+            System.out.println(nome + " sem fome");
+        }
     }
 
     void dormir() {
-        System.out.println(nome + " dormindo");
+        if (sono >= 1) {
+            System.out.println(nome + " dormindo");
+            sono -= 1;
+            energia = energia + 1 <= 10 ? energia + 1 : 10;
+        } else {
+            System.out.println(nome + " sem sono");
+        }
     }
 }
