ad3d361862c3eb18a0a86fad717c1f2cb8cdbf87
Gabriel <gabriellippidacosta@gmail.com>
2025-05-22 19:23:45 -0300

    Método exibir mensagem está pronta

--- modelo/LivroDeNotas.java
+++ modelo/LivroDeNotas.java
@@ -1,7 +1,18 @@
 package modelo;
 
 public class LivroDeNotas {
+
+    private String nomeDoCurso;
+
     public void exibirMensagem() {
-        System.out.println("Bem-vindo ao livro de notas");
+        System.out.printf("Bem-vindo ao livro de notas do curso %s ", getNomeDoCurso());
+    }
+
+    public String getNomeDoCurso() {
+        return nomeDoCurso;
+    }
+
+    public void setNomeDoCurso(String nomeDoCurso) {
+        this.nomeDoCurso = nomeDoCurso;
     }
 }
--- tela/TesteLivroNotas.java
+++ tela/TesteLivroNotas.java
@@ -1,11 +1,16 @@
 package tela;
 
+import javax.swing.JOptionPane;
+
 import modelo.LivroDeNotas;
 
 public class TesteLivroNotas {
     public static void main(String[] args) {
         var livroDeNotas = new LivroDeNotas();
 
+        String nomeDoCurso = JOptionPane.showInputDialog("Professor, qual o nome do curso? ");
+
+        livroDeNotas.setNomeDoCurso(nomeDoCurso);
         livroDeNotas.exibirMensagem();
     }
 }
