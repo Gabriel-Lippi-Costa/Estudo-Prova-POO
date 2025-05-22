b38cd78a89393b1e6431346f3712293cd233c825
Gabriel <gabriellippidacosta@gmail.com>
2025-05-22 19:34:04 -0300

    Criei a classe personagem

---
+++ jogo/Personagem.java
@@ -0,0 +1,20 @@
+package jogo;
+
+public class Personagem {
+    String nome;
+    int energia;
+    int fome;
+    int sono;
+
+    void cacar() {
+        System.out.println(nome + "caçando");
+    }
+
+    void comer() {
+        System.out.println(nome + "comendo");
+    }
+
+    void dormir() {
+        System.out.println(nome + "dormindo");
+    }
+}
