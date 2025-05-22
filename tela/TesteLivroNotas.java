018d852cada7356fa061f8271aa56269f2e7ad7c
Gabriel <gabriellippidacosta@gmail.com>
2025-05-22 19:14:58 -0300

    Classe TesteLivroNotas foi criada

---
+++ tela/TesteLivroNotas.java
@@ -0,0 +1,11 @@
+package tela;
+
+import modelo.LivroDeNotas;
+
+public class TesteLivroNotas {
+    public static void main(String[] args) {
+        var livroDeNotas = new LivroDeNotas();
+
+        livroDeNotas.exibirMensagem();
+    }
+}
