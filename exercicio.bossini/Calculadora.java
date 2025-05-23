97076e5e437f2cffebbb2a4579a02a4df822da3c
Gabriel <gabriellippidacosta@gmail.com>
2025-05-23 08:40:51 -0300

    Exercício Bossini feito

---
+++ exercicio.bossini/Calculadora.java
@@ -0,0 +1,11 @@
+public class Calculadora {
+    
+    void subtrair(int a, int b) {
+        System.out.println(a - b);
+    }
+
+    int multiplicacao(int a, int b) {
+        return a *b;
+    }
+
+}
